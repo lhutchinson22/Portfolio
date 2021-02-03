b980bc3fdc66e12fd41c4b89039d9a1498a1cdd7
lhutchinson22 <lbhutchinson022@gmail.com>
2021-02-02 17:10:57 -0800

    parallax added + media queries + whole new css design

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,6 +5,6 @@ var path = require("path");
 module.exports = (app) => {
   // index route loads view.html
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/landing.html"));
+    res.sendFile(path.join(__dirname, "./../public/landing.html"));
   });
 };
